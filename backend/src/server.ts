42ac4558cbbc3147021cde8e1ec7a7a2ca3e8ef3
Jeetu Pal <jeetupal.pal31@gmail.com>
2026-03-28 23:47:54 +0530

    feat: enable cors for frontend-backend communication

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 import express from "express"
+import cors from "cors";
 import { runWorkflow } from "./engine/executionEngine"
 
 const app = express()
 
+app.use(cors());
 app.use(express.json())
 
 app.get("/", (req,res)=>{
